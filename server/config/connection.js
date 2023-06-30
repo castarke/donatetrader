18b326e7ad18837af083cc5e0374617ae991c7a6
Dewey Watkins <deweywatkins77@gmail.com>
2023-06-29 20:00:42 -0400

    setup initial config.js

---
+++ server/config/connection.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const { connect, connection } = require('mongoose');
+const connectionString = process.env.MONGO_URI || 'mongodb://root:root@localhost:27017/?authMechanism=DEFAULT&authSource=admin'
+
+connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = connection;
