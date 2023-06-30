a06a16c6a0f1e0e92137eda0cf44de2169db1ccc
Sandy Bui <stbui_emory@yahoo.com>
2023-06-30 02:32:00 -0400

    added client side home js and landing page js

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,1 +1,32 @@
-// 
+import React from 'react';
+
+export default function LandingPage() {
+    return (
+        <div>
+            <div className='About'>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                </p>
+            </div>
+            <div className='Steps'>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                </p>
+            </div>
+            <div className='SignUp'>
+                <button>
+                    SignUp
+                </button>
+            </div>
+            <div className='LogIn'>
+                <button>
+                    LogIn
+                </button>
+            </div>
+        </div>
+    )
+}
