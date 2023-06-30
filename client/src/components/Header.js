f99ceee7b70b04f13395c48aacaec23f0d44b831
castarke <candler.starke@yahoo.com>
2023-06-29 21:18:24 -0400

    working on header, pulling from main

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,1 +1,28 @@
-// 
+import React , {useState} from 'react';
+import Navigation from './Navbar';
+import Account from '../pages/Account';
+import Donations from '../pages/Donations';
+import Home from '../pages/Home';
+import Items from '../pages/Items';
+
+function Header() {
+    const [currentPage, handlePageChange] = useState('Home');
+
+    const renderPage = () => {
+        switch (currentPage) {
+            case 'Home':
+                return <Home />;
+            case 'Items':
+                return <Items />;
+            case 'Donations':
+                return <Donations />
+            case 'Account':
+                return <Account /> 
+            
+            default:
+                return <Home />
+        }
+    };
+
+    
+}
