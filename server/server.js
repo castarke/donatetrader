9189111ead00e3b1b83d76dfe0a27c116dd079b8
castarke <candler.starke@yahoo.com>
2023-07-04 14:05:09 -0400

    fixed bugs to allow local host to render

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const {ApolloServer}=require('apollo-server-express');
 const path=require('path');
-const{typeDefs,resolvers}=require('./schemas');
+const{typeDefs,resolvers}=require('./schema');
 const db= require('./config/connection');
 const PORT=process.env.PORT||3001;
 const app=express();
