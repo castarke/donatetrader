54763dc2737d0d423d2319fe835f91933d15484e
Dewey Watkins <deweywatkins77@gmail.com>
2023-07-12 10:01:14 -0400

    minor updates for search component and image api

--- client/src/components/SearchCriteria.js
+++ client/src/components/SearchCriteria.js
@@ -43,11 +43,11 @@ const SearchCriteria = () => {
   };
 
   const handleInputChange = (e) => {
-    setSearch({
-      ...searchCriteria,
-      [e.target.name]: e.target.value
-    });
-    console.log(searchCriteria)
+    const { name, value } = e.target;
+    setSearch((prevSearchCriteria) => ({
+      ...prevSearchCriteria,
+      [name]: value,
+    }));
   };
 
   if (loadingCategory || searchItemsLoading) return <p>Loading...</p>;
@@ -62,20 +62,20 @@ const SearchCriteria = () => {
         value={searchCriteria.searchText}
         name="searchText"
         onChange={handleInputChange}
-        // fullWidth
+        fullWidth
         variant="outlined"
         margin="normal"
       />
 
       <FormControl fullWidth variant="outlined" margin="normal">
         <InputLabel>Price</InputLabel>
-        <Select name="value" value={searchCriteria.value} onChange={handleInputChange}>
+        <Select value={searchCriteria.value || ""} name="value"  onChange={handleInputChange}>
             <MenuItem value={[0,25]}>$0-$25</MenuItem>
             <MenuItem value={[26,50]}>$26-$50</MenuItem>
             <MenuItem value={[51,100]}>$51-$100</MenuItem>
             <MenuItem value={[101,500]}>$101-$500</MenuItem>
             <MenuItem value={[500,1000]}>$501-$1000</MenuItem>
-            <MenuItem value={[1001,1000000]}>&gt1000</MenuItem>
+            <MenuItem value={[1001,1000000]}>&gt;1000</MenuItem>
         </Select>
       </FormControl>
 
@@ -83,7 +83,7 @@ const SearchCriteria = () => {
         <InputLabel>Category</InputLabel>
         <Select
           name="categories"
-          value={searchCriteria.categories}
+          value={searchCriteria.categories || ""}
           onChange={handleInputChange}
         >
           {categoryArr.map((item) =>(
@@ -100,22 +100,3 @@ const SearchCriteria = () => {
 };
 
 export default SearchCriteria;
-      {/* 
-      
-
-
-
-      <TextField
-        label="Location"
-        value={location}
-        onChange={(e) => setLocation(e.target.value)}
-        fullWidth
-        variant="outlined"
-        margin="normal"
-      />
-
-      <Button type="submit" variant="contained" color="primary">
-        Search
-      </Button>
-    </form>
-  ); */}
