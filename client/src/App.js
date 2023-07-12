e30f5e29b3389162a90cb2bc83cb4df050c8b301
castarke <candler.starke@yahoo.com>
2023-07-12 02:25:53 -0400

    fixed so navbar is only displayed once logged in

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
 import Navbar from "./components/Navbar";
@@ -13,9 +13,8 @@ import Search from './pages/Search';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import ItemPage from './pages/ItemPage'
-import AuthService, {Auth, AuthProvider} from './utils/auth';
+import AuthService, { Auth, AuthProvider } from './utils/auth';
 import LandingPage from './pages/LandingPage';
-
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:3001/graphql'
@@ -39,25 +38,27 @@ const client = new ApolloClient({
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(AuthService.loggedIn());
 
+  useEffect(() => {
+    setIsLoggedIn(AuthService.loggedIn());
+  }, []);
+
   return (
     <ApolloProvider client={client}>
-      {/* <AuthProvider> */}
-        <Router>
-          <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
-          <Routes>
-            <Route path='/' element={<LandingPage />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/gallery" element={<Gallery />} />
-            <Route path="/account" element={<AccountInfo />} />
-            <Route path="/additem/:ownerId" element={<AddItem />} />
-            <Route path="/updateitem/:itemId" element={<UpdateItemForm />} />
-            <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
-            <Route path="/signup" element={<Signup setIsLoggedIn={setIsLoggedIn} />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/item/:itemId" element={<ItemPage />} />
-          </Routes>
-        </Router>
-      {/* </AuthProvider> */}
+      <Router>
+        {isLoggedIn && <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />}
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/account" element={<AccountInfo />} />
+          <Route path="/additem/:ownerId" element={<AddItem />} />
+          <Route path="/updateitem/:itemId" element={<UpdateItemForm />} />
+          <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+          <Route path="/signup" element={<Signup setIsLoggedIn={setIsLoggedIn} />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/item/:itemId" element={<ItemPage />} />
+        </Routes>
+      </Router>
     </ApolloProvider>
   );
 }
