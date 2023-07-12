0c88421e6cb9c853da2e22bf3060005b8ca411f0
Sandy Bui <stbui_emory@yahoo.com>
2023-07-12 02:49:20 -0400

    "app.js merge"

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
 import React, { useState } from 'react';
 import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import Home from './pages/Home';
 import AccountInfo from './pages/Account';
 import AddItem from './pages/AddItem'
 import { setContext } from '@apollo/client/link/context';
-import SearchCriteria from './components/SearchCriteria';
 import Gallery from './components/Gallery';
-import RecentTrades from './components/RecentTrades';
 import { Contact } from './pages/Contact'
 import UpdateItemForm from './pages/MyAccountPages/UpdateItem';
 import Search from './pages/Search';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import ItemPage from './pages/ItemPage'
-import Auth from './utils/auth';
 import About from './pages/About';
 import LandingPage from './pages/LandingPage'
+import Auth from './utils/auth';
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:3001/graphql'
@@ -39,29 +37,29 @@ const client = new ApolloClient({
 });
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(Auth.loggedIn());
+  const [isLoggedIn, setIsLoggedIn] = useState(AuthService.loggedIn());
 
   return (
     <ApolloProvider client={client}>
-      <Router>
-        <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/account" element={<AccountInfo />} /> 
-          <Route path='/login' element={<Login setIsLoggedIn={setIsLoggedIn} />} />
-          <Route path='/signup' element={<Signup setIsLoggedIn={setIsLoggedIn} />} />
-          <Route path ="/contact" element={<Contact />} />
-          <Route path ="/item/:itemId" element={<ItemPage />} />
-          <Route path="/additem/:ownerId" element={<AddItem />} />
-          <Route path="/updateitem/:itemId" element={<UpdateItemForm />} />
-          <Route path="/search/:searchCriteria" element={<Search />} />
-        </Routes>
-        {/* <SearchCriteria /> */}
-        {/* <RecentTrades /> */}
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/account" element={<AccountInfo />} />
+            <Route path="/additem/:ownerId" element={<AddItem />} />
+            <Route path="/updateitem/:itemId" element={<UpdateItemForm />} />
+            <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+            <Route path="/signup" element={<Signup setIsLoggedIn={setIsLoggedIn} />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/item/:itemId" element={<ItemPage />} />
+            <Route path="/search/:searchCriteria" element={<Search />} />
+          </Routes>
+        </Router>
+      </AuthProvider>
     </ApolloProvider>
   );
-};
+}
 
 export default App;
