551e97c009f1bf27f18360f67571a0327e092fdd
castarke <candler.starke@yahoo.com>
2023-07-12 21:27:04 -0400

    fixed logout function for the drawer and fixed bug where there were multiple logout buttons

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   AppBar,
   Toolbar,
@@ -9,113 +10,102 @@ import {
 import { NavLink, Link } from "react-router-dom";
 import DrawerComponent from "./Drawer";
 import Logo from "./Logo";
-import {useStyles} from '../utils/makeStyles';
-import auth from '../utils/auth';
-
+import { useStyles } from "../utils/makeStyles";
+import auth from "../utils/auth";
 
 function Navbar() {
   const classes = useStyles();
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
-  const NavigationLinks = [
-    <NavLink
-      exact
-      to="/login"
-      className={classes.link}
-      activeClassName="active"
-      // style={linkStyle}
-      key="login"
-    >
-      Login
-    </NavLink>,
-    <NavLink
-      exact
-      to="/signup"
-      className={classes.link}
-      activeClassName="active"
-      // style={linkStyle}
-      key="signup"
-    >
-      Signup
-    </NavLink>,
-    <NavLink
-      exact
-      to="/home"
-      className={classes.navLink}
-      activeClassName="active"
-      key="home">
-        Home
-    </NavLink>,
-    <NavLink
-      exact
-      to="/gallery"
-      className={classes.navLink}
-      activeClassName="active"
-      key="gallery">
-      Items for trade
-    </NavLink>,
-    <NavLink
-      exact
-      to="/account"
-      className={classes.navLink}
-      activeClassName="active"
-      key="account">
-      My Account
-    </NavLink>,
-    <NavLink
-      exact
-      to="/donations"
-      className={classes.navLink}
-      activeClassName="active"
-      key="donations">
-      Donations
-    </NavLink>,
-    <NavLink
-      exact
-      to="/about"
-      className={classes.navLink}
-      activeClassName="active"
-      key="about">
-      About
-    </NavLink>,
-    <NavLink
-      exact
-      to="/contact"
-      className={classes.navLink}
-      activeClassName="active"
-      key="contact">
-      Contact
-    </NavLink>,
-    // <NavLink
-    //   exact
-    //   to="/logout"
-    //   className={classes.navLink}
-    //   activeClassName="active"
-    //   // style={linkStyle}
-    //   key="gallery"
-    // >
-    //   Items for trade
-    // </NavLink>,
-  ];
   const handleLogout = () => {
     auth.logout();
   };
-  const logoutLink = <NavLink 
-      exact
-      onClick={handleLogout}
-      className={classes.navLink}   
-      activeClassName="active"
-      // style={linkStyle}
-      key="account">
-    Logout
-  </NavLink>
-console.log(NavigationLinks)
-if(auth.loggedIn()) {
-  NavigationLinks.splice(0,2,logoutLink)
-}
+
+  const renderNavigationLinks = () => {
+    if (auth.loggedIn()) {
+      return [
+        <NavLink
+          exact
+          to="/home"
+          className={classes.navLink}
+          activeClassName="active"
+          key="home"
+        >
+          Home
+        </NavLink>,
+        <NavLink
+          exact
+          to="/gallery"
+          className={classes.navLink}
+          activeClassName="active"
+          key="gallery"
+        >
+          Items for trade
+        </NavLink>,
+        <NavLink
+          exact
+          to="/account"
+          className={classes.navLink}
+          activeClassName="active"
+          key="account"
+        >
+          My Account
+        </NavLink>,
+        <NavLink
+          exact
+          to="/donations"
+          className={classes.navLink}
+          activeClassName="active"
+          key="donations"
+        >
+          Donations
+        </NavLink>,
+        <NavLink
+          exact
+          to="/about"
+          className={classes.navLink}
+          activeClassName="active"
+          key="about"
+        >
+          About
+        </NavLink>,
+        <NavLink
+          exact
+          to="/contact"
+          className={classes.navLink}
+          activeClassName="active"
+          key="contact"
+        >
+          Contact
+        </NavLink>
+      ];
+    } else {
+      return [
+        <NavLink
+          exact
+          to="/login"
+          className={classes.link}
+          activeClassName="active"
+          key="login"
+        >
+          Login
+        </NavLink>,
+        <NavLink
+          exact
+          to="/signup"
+          className={classes.link}
+          activeClassName="active"
+          key="signup"
+        >
+          Signup
+        </NavLink>
+      ];
+    }
+  };
+
   return (
-    <AppBar position="static" style ={{backgroundColor: '#66b2b2'}}>
+    <AppBar position="static" style={{ backgroundColor: "#66b2b2" }}>
       <CssBaseline />
       <Toolbar>
         <Typography variant="h4" className={classes.navLogo}>
@@ -124,9 +114,23 @@ if(auth.loggedIn()) {
           </Link>
         </Typography>
         {isMobile ? (
-          <DrawerComponent links={NavigationLinks} />
+          <DrawerComponent links={renderNavigationLinks()} handleLogout={handleLogout} />
         ) : (
-          <div className={classes.navLinks}>{NavigationLinks}</div>
+          <div className={classes.navLinks}>
+            {renderNavigationLinks()}
+            {auth.loggedIn() && (
+              <NavLink
+                exact
+                to="/logout"
+                onClick={handleLogout}
+                className={classes.navLink}
+                activeClassName="active"
+                key="logout"
+              >
+                Logout
+              </NavLink>
+            )}
+          </div>
         )}
       </Toolbar>
     </AppBar>
